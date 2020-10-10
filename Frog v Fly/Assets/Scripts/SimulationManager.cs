55fc12755c23cda6f2ff012dba1b618fb2066e9c
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2020-10-10 05:58:22 -0400

    Update SimulationManager.cs

--- Frog v Fly/Assets/Scripts/SimulationManager.cs
+++ Frog v Fly/Assets/Scripts/SimulationManager.cs
@@ -26,14 +26,11 @@ public class SimulationManager : MonoBehaviour
     [ReadOnly]
     public GameObject fly;
 
-    // time sim has been running
+    
     private float _time = 0;
-    // number of flies the frog has caught
     private float _numOfFliesCaught = 0;
-    // how efficient the frog is
     private float _frogEfficiencyValue;
 
-    // objects on the user interface
     #region UserInterfaceObjects
 
         [Header("User Interface")]
@@ -52,12 +49,9 @@ public class SimulationManager : MonoBehaviour
 
     private void Start()
     {
-        // get a random position on screen and create a fly there
         Vector3 position = new Vector3(Random.Range(-9, 9), Random.Range(-5, 5), 0);
         fly = (GameObject) Instantiate(flyPrefab, position, Quaternion.identity);
 
-
-        // get all the user interface objects during run time
         #region GetUserInterfaceObjects
 
         GameObject infoPanel = simInterface.transform.GetChild(0).gameObject;
@@ -74,7 +68,6 @@ public class SimulationManager : MonoBehaviour
 
         #endregion
 
-        // initialize all the text UI objects
         _timer.text = "Time: 0 s";
         _frogMoves.text = "Moves: 0";
         _flyDistance.text = "Fly Distance: 0";
@@ -83,48 +76,36 @@ public class SimulationManager : MonoBehaviour
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // if there's no fly on the scene, make a new one
         if (flyActive == false)
         {
-            // update the number of flies caught
             _numOfFliesCaught += 1;
             _fliesCaught.text = "Flies Caught: " + _numOfFliesCaught;
 
-            // get a random position on screen and create a fly there
             Vector3 position = new Vector3(Random.Range(-9, 9), Random.Range(-5, 5), 0);
             fly = (GameObject) Instantiate(flyPrefab, position, Quaternion.identity);
             flyActive = true;
         }
         else
         {
-            // update the frog's efficiency
             _frogEfficiencyValue = _numOfFliesCaught /_time;
             _frogEfficiency.text = "Efficiency: " + _frogEfficiencyValue + " flies\\sec";
         }
-
-        // update the timer and frog's number of moves
         UpdateTimerUI();
         _frogMoves.text = "Moves: " + frogMovesValue;
 
     }
-
-    // update the timer
     void UpdateTimerUI(){
-        //set timer UI
         _time += Time.deltaTime;
         _timer.text = "Time: " + _time + "s";
     }
 
-    // update the distance between the fly and the frog
     public void UpdateDistance(float distance)
     {
         _flyDistance.text = "Fly Distance: " + distance;
     }
 
-    // restart the simulation
     private void RestartSim()
     {
         SceneManager.LoadScene(1);
